d8c9a008d6ab19fc7f8b9790102c2610dd2b7c36
agamez <alejandro-gamez@outlook.com>
2021-05-02 00:39:12 -0500

    Add book validations

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,13 +12,16 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
 
-
     respond_to do |format|
       if @book.save
         format.html  { redirect_to(books_path,
                       :notice => 'Book was successfully created.') }
         format.json  { render :json => @book,
                       :status => :created, :location => @book }
+      else
+        format.html  { render :action => "new" }
+        format.json  { render :json => @book.errors,
+                      :status => :unprocessable_entity }
       end
     end
 
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,7 @@
 class Book < ApplicationRecord
+  validates_length_of :title, minimum: 3, maximum: 25
+  validates_uniqueness_of :title
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :genre, presence: true
 end
