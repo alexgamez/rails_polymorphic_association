90a2c92f5a93ce712d79fa42048c83bb6e2cbe8a
agamez <alejandro-gamez@outlook.com>
2021-05-02 14:15:06 -0500

    Process to update a magazine

--- app/controllers/magazines_controller.rb
+++ app/controllers/magazines_controller.rb
@@ -1,4 +1,6 @@
 class MagazinesController < ApplicationController
+  before_action :set_magazine, only: [:edit, :update]
+  
   def index
     @magazines =Magazine.all
   end
@@ -22,11 +24,30 @@ class MagazinesController < ApplicationController
   end
 
   def edit
+   
+  end
+
+  def update
+    @magazine.assign_attributes(magazine_params)
+
+    respond_to do |format|
+      if @magazine.save
+        format.html do
+          redirect_to(magazines_path, notice: 'Magazine was successfully updated.')
+        end
+      else
+        format.html { render action: "edit"}
+      end
+    end
   end
 
   private
 
   def magazine_params
     params.require(:magazine).permit(:title, :target_audiences, :description)
+  end
+
+  def set_magazine
+    @magazine = Magazine.find(params[:id])
   end
 end
