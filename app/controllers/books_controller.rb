d2dda93b7db89da4a8319191a5ac841e7edce842
agamez <alejandro-gamez@outlook.com>
2021-05-02 00:14:39 -0500

    Process to create a new book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,8 +6,29 @@ class BooksController < ApplicationController
   end
 
   def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+
+    respond_to do |format|
+      if @book.save
+        format.html  { redirect_to(@book,
+                      :notice => 'Book was successfully created.') }
+        format.json  { render :json => @book,
+                      :status => :created, :location => @book }
+      end
+    end
+
   end
 
   def edit
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(%i[title author genre])
   end
 end
